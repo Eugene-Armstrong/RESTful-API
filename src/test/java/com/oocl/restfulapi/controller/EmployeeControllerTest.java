2f9b518ed0a55f0e664bb6db051e9fc2539ba9d5
JEFFERY LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUJE@OOCL.COM>
2018-07-28 21:16:14 +0800

    [Jeffery] add should_get_all_employees test

--- src/test/java/com/oocl/restfulapi/controller/EmployeeControllerTest.java
+++ src/test/java/com/oocl/restfulapi/controller/EmployeeControllerTest.java
@@ -43,26 +43,22 @@ public class EmployeeControllerTest {
     private ObjectMapper mapper;
 
     @Test
-    public void should_get_all_employees(){
+    public void should_get_all_employees() throws Exception{
         //given
         Employee employee1 = new Employee(0, "Jason", 22,"male",5000);
         Employee employee2 = new Employee(1, "Rose", 20,"female",4000);
         List<Employee> employees = Arrays.asList(employee1, employee2);
         //when
         when(employeeServiceImpl.getEmployeeList()).thenReturn(employees);
-        try {
-            ResultActions resultActions = mockMvc.perform(get("employees"));
-            //then
-            resultActions.andExpect(status().isOk())
-                    .andExpect(jsonPath("$", hasSize(2)))
-                    .andExpect(jsonPath("$[0].content.id", is(0)))
-                    .andExpect(jsonPath("$[0].content.name", containsString("Jason")))
-                    .andExpect(jsonPath("$[0].content.age", is(22)))
-                    .andExpect(jsonPath("$[0].content.gender", is("male")))
-                    .andExpect(jsonPath("$[0].content.salary", is(5000)))
-                    .andExpect(jsonPath("$[1].content.id", is(1)));
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        ResultActions resultActions = mockMvc.perform(get("/employees"));
+        //then
+        resultActions.andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id", is(0)))
+                .andExpect(jsonPath("$[0].name", containsString("Jason")))
+                .andExpect(jsonPath("$[0].age", is(22)))
+                .andExpect(jsonPath("$[0].gender", is("male")))
+                .andExpect(jsonPath("$[0].salary", is(5000)))
+                .andExpect(jsonPath("$[1].id", is(1)));
     }
 }
